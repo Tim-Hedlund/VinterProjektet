7aad9a5d5bc3cbabb4c8be16ed54165787de024d
Tim-Hedlund <tim.hedlund@elev.ga.ntig.se>
2023-12-04 09:17:59 +0100

    Third

--- src/Main.java
+++ src/Main.java
@@ -34,8 +34,6 @@ public class Main {
             start();
             Map map = createMap();
 
-            
-
         }
 
     }
--- src/Map.java
+++ src/Map.java
@@ -1,15 +1,29 @@
 public class Map {
     private int[][] map;
 
-    private void printMap(int[][] map) {
+    private void changeValue(int x, int y, int value) {
 
-        int width = map.length;
-        int height = map[0].length;
+        if (this.map.length >= y && y >= 0) {
+            if (this.map[0].length >= x && x >= 0) {
+                this.map[y][x] = value;
+                return;
+            }
+        }
 
-        for (int i = 0; i < width; i++) {
+        System.out.println("ERROR, Out of bounds");
+        System.out.print("X = " + x);
+        System.out.println(" : Y = " + y);
+    }
+
+    private void printMap() {
+        int height = this.map.length;
+        int width = this.map[0].length;
+
+        for (int i = 0; i < height; i++) {
             System.out.println();
-            for (int j = 0; j < height; j++) {
-                System.out.print(map[i][j]);
+            for (int j = 0; j < width; j++) {
+                System.out.print(this.map[i][j]);
+
             }
 
         }
@@ -17,18 +31,19 @@ public class Map {
     }
 
     public Map(int mapDefaultValue, int height, int width) {
+        this.map = new int[height][width];
 
-        this.map = new int[width][height];
-
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
                 this.map[i][j] = mapDefaultValue;
             }
-
         }
 
-        printMap(this.map);
+        changeValue(0, 0, 1);
+        changeValue(1, 2, 2);
+        changeValue(2, 4, 3);
+        changeValue(-1, -1 , -1);
 
+        printMap();
     }
-
 }
